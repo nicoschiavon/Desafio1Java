28f62dea09a2e5036264854f11022c9443541ee0
nicoschiavon <nico.schiavon@gmail.com>
2025-04-08 18:22:53 -0300

    first commit

---
+++ src/Desafio.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nombre = "Tony Stark";
+        String tipoCuenta = "Corriente";
+        double saldo = 1599.99;
+
+        System.out.println("Nombre del cliente");
+    }
+}
